ae2128ace34c1c4d9ad183a9ec592fc18e2b3e8f
Alhamin-Ibrahim <alhaminibrahim1@gmail.com>
2026-09-09 18:11:31 +0100

    unit test update

--- tests/unit/test_my_cdk_app_stack.py
+++ tests/unit/test_my_cdk_app_stack.py
@@ -1,7 +1,7 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from my_cdk_app.my_cdk_app_stack import MyCdkAppStack
+from my_cdk_app.infra_stack import MyCdkAppStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in my_cdk_app/my_cdk_app_stack.py
